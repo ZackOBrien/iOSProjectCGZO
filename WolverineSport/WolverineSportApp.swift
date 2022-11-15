22aec30a83301fc32816595b28d1e6dedbba1816
ZackOBrien <obrienzj19@gcc.edu>
2022-11-14 20:17:42 -0500

    Initial Commit

---
+++ WolverineSport/WolverineSportApp.swift
@@ -0,0 +1,17 @@
+//
+//  WolverineSportApp.swift
+//  WolverineSport
+//
+//  Created by Zack O'Brien on 11/14/22.
+//
+
+import SwiftUI
+
+@main
+struct WolverineSportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
